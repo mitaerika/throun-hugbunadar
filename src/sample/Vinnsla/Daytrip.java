c6dbffc5cb47070b1d4ad2cd896213eb7d6ac524
Ásdís Valtýsdóttir <asdisval@gmail.com>
2023-03-01 16:01:04 +0000

    Bætti við this í tilviksbreytur

--- src/sample/Vinnsla/Daytrip.java
+++ src/sample/Vinnsla/Daytrip.java
@@ -13,14 +13,14 @@ public class Daytrip {
     int available_seats;
     //Vantar ennþá activity, location, review og hotel.
     public void Datrip(String dTitle1, Date date1, Date start_time1, Date end_time1, String description1, int price1, String photo1, int available_seats1) {
-        dTitle = dTitle1;
-        date = date1;
-        start_time = start_time1;
-        end_time = end_time1;
-        description = description1;
-        price = price1;
-        photo = photo1;
-        available_seats = available_seats1;
+        this.dTitle = dTitle1;
+        this.date = date1;
+        this.start_time = start_time1;
+        this.end_time = end_time1;
+        this.description = description1;
+        this.price = price1;
+        this.photo = photo1;
+        this.available_seats = available_seats1;
     }
 }
 
