af0e28f8cdc1474015f77e521bdd965cb13f984d
jollipjolli <jfd3@hi.is>
2023-03-08 11:34:38 +0000

    Fullt af drasli í Daytrip.java

--- src/sample/Vinnsla/Daytrip.java
+++ src/sample/Vinnsla/Daytrip.java
@@ -13,7 +13,8 @@ public class Daytrip {
     int available_seats;
     String location;
     Review[] reviews;
-    //Vantar ennþá activity, location, review og hotel.
+    final String[] activity = new String[]{"Activity1", "Activity2", "Activity3"};
+    final String[] hotel = new String[]{"Hotel1", "Hotel2", "Hotel3"};
     public Daytrip(String dTitle1, Date date1, Date start_time1, Date end_time1, String description1, int price1, String photo1, int available_seats1, String location1, Review[] reviews) {
         this.dTitle = dTitle1;
         this.date = date1;
@@ -46,6 +47,18 @@ public class Daytrip {
     }
     public int getAvailable_seats() {
         return this.available_seats;
+    }
+    public String[] getActivity() {
+        return activity;
+    }
+    public String[] getHotel() {
+        return hotel;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public Review[] getReview(){
+        return reviews;
     }
 
 }
