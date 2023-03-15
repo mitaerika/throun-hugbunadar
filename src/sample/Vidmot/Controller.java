dcddcf9b9143e2fa7d01bf0aa7063f54208677cb
jollipjolli <jfd3@hi.is>
2023-03-15 15:28:52 +0000

    SortByRating í DaytripController.java og smá breytingar í Daytrip.java

--- src/sample/Vidmot/Controller.java
+++ src/sample/Vidmot/Controller.java
@@ -10,8 +10,5 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Controller {
-    String l = "Hæ";
-    int lol = 1;
-    Date fuck = new Date(1, 1, 1);
-    Daytrip test = new Daytrip(l, fuck, fuck, fuck, l, lol, l, lol, l, l);
+
 }
