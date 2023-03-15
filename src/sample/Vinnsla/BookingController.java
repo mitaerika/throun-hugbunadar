72ee65fcaec5d589ae231b4c9d2f6bde037924b6
Ásdís Valtýsdóttir <asdisval@gmail.com>
2023-03-15 10:57:31 +0000

    kommentaðar aðferðir í booking controller

--- src/sample/Vinnsla/BookingController.java
+++ src/sample/Vinnsla/BookingController.java
@@ -1,5 +1,13 @@
 package sample.Vinnsla;
 
 public class BookingController {
+    /* public Booking addBooking(Daytrip daytrip, Customer customer){
+    } */
+
+    /*public Booking removeBooking(Daytrip daytrip){
+        return Booking;
+    }
+    */
+
 
 }
