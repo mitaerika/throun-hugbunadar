09c5c994149a976ecaaffd0a0e69b3f8aa1cee5b
mitaerika <72701220+mitaerika@users.noreply.github.com>
2023-03-25 19:17:08 +0000

    Changed daytrip photo from String to Image

--- src/sample/Vinnsla/Daytrip.java
+++ src/sample/Vinnsla/Daytrip.java
@@ -1,5 +1,6 @@
 package sample.Vinnsla;
 
+import javafx.scene.image.Image;
 import sample.Vidmot.DaytripController;
 
 import java.text.SimpleDateFormat;
@@ -14,7 +15,7 @@ public class Daytrip extends DaytripController {
     LocalTime end_time;
     String description;
     int price;
-    String photo;
+    Image photo;
     int available_seats;
     String location;
     String[] reviews;
@@ -28,7 +29,7 @@ public class Daytrip extends DaytripController {
         this.end_time = end_time1;
         this.description = description1;
         this.price = price1;
-        this.photo = photo1;
+        this.photo = new Image(photo1);
         this.available_seats = available_seats1;
         this.location = location1;
         this.reviews = reviews;
